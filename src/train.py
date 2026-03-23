04e4cca2ebf03900e2059b6f2f5cb4399db91cfb
Kaan Guensoy <kguensoy@stud.hs-heilbronn.de>
2026-03-23 16:12:56 +0100

    first training

--- src/train.py
+++ src/train.py
@@ -1,19 +1,30 @@
 from ultralytics import YOLO
 import torch
+import os
 
 def main():
     # Für Cuda Training 
     device = 0 if torch.cuda.is_available() else "cpu"
-
-    model = YOLO("yolov8l.pt")  
     
-   
+    my_model_path = "models/yolo_train/weights/best.pt"
+    
+    if not os.path.exists(my_model_path) or os.path.getsize(my_model_path) == 0:
+        print("Kein eigenes Modell gefunden. Starte mit YOLOv8l Basis-Modell...")
+        model = YOLO("yolov8l.pt") 
+    else:
+        print(f"Lade existierendes Modell: {my_model_path}")
+        model = YOLO(my_model_path)
+
     model.train(
         data="dataset/config.yaml", 
-        epochs=50, 
+        epochs=15,
         imgsz=640, 
         device=device,
-        workers=4 
+        workers=2,
+        project="models", 
+        name="yolo_train",
+        exist_ok=True,
+        patience=5
     )
 
 if __name__ == "__main__":
